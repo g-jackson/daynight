afd7e46393bac203131a90fa8a5680960fdc763f
james0048 <jdjohnson0048@gmail.com>
2019-09-17 08:27:37 -0700

    New blocks

---
+++ DayNight/Assets/GridGizmos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridGizmos : MonoBehaviour
+{
+    public Tilemap tilemap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Gizmos.DrawSphere(tilemap.origin, .1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
